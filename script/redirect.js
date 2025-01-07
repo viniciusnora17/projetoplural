3138b93f56bc79b7f147442d3e4af63b2addc315
Vinicius Nora <vinicius.nora@sou.fae.br>
2025-01-06 22:44:03 -0300

    Responsividade da home/ preciso terminar footer de contatos

--- script/redirect.js
+++ script/redirect.js
@@ -1,3 +1,11 @@
 function redirect(){
     location.replace("index.html")
 }
+
+function redirectInsta(){
+    location.replace("https://www.instagram.com/pluralconstroe/")
+}
+
+function redirectFace(){
+    location.replace("https://www.facebook.com/pluralconstroe?locale=pt_BR")
+}
