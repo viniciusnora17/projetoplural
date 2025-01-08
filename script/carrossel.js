7109a11ee41710262451ce119d541c984308cab7
Vinicius Nora <vinicius.nora@sou.fae.br>
2025-01-07 21:16:23 -0300

    Funcionamento do carrossel/criação de projetos e sobre

---
+++ script/carrossel.js
@@ -0,0 +1,26 @@
+const imagens = [
+    "img/cristiano.jpg",
+    "img/cris.jpeg"
+];
+
+const imagemCarrossel = document.querySelector('.img-carrossel');
+const botaoProximo = document.querySelector('.text-carrossel');
+
+let indiceAtual = 0;
+
+botaoProximo.addEventListener('click', () => {
+    
+    indiceAtual++;
+
+    if (indiceAtual >= imagens.length + 1) {
+        indiceAtual = 0;
+    }
+
+    if (indiceAtual === 0) {
+  
+        imagemCarrossel.src = "img/teste.jpg";
+    } else {
+
+        imagemCarrossel.src = imagens[indiceAtual - 1];
+    }
+});
