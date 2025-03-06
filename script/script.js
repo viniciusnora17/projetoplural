40fa648fe9fdc88f253494ddd1163806f1282dce
Vinicius Nora <vinicius.nora@sou.fae.br>
2025-03-06 16:37:03 -0300

    Atualização pag projetos

--- script/script.js
+++ script/script.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
+
     const sobreNos = document.querySelector("#sobre-nos");
 
     const observer = new IntersectionObserver(
@@ -32,4 +33,46 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     observer.observe(sobreNos);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    let title = document.querySelector(".container-texto h2");
+    let text = title.innerText;
+    title.innerHTML = ""; 
+
+    let delay = 0; 
+    text.split("").forEach((letter) => {
+        let span = document.createElement("span");
+        span.innerText = letter;
+        
+        if (letter !== " ") {
+            span.style.animation = `fadeInLetter 0.1s ease-in forwards`;
+            span.style.animationDelay = `${delay}s`;
+            delay += 0.1; 
+        }
+
+        title.appendChild(span);
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".segundo-container");
+    const texto = document.querySelector(".containersegundo-texto");
+    const imagem = document.querySelector(".silas-samuel");
+
+    function checkScroll() {
+        const posicao = container.getBoundingClientRect().top;
+        const alturaTela = window.innerHeight;
+
+        if (posicao < alturaTela * 0.75) { // Quando 75% do container estiver visível
+            texto.classList.add("animar-esquerda");
+            imagem.classList.add("animar-direita");
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll(); // Para verificar se a seção já está visível ao carregar a página
 });
