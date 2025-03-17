92efa025a247e744ff1b48dc58f3d8d914c8e3c9
Vinicius Nora <vinicius.nora@sou.fae.br>
2025-03-17 19:57:57 -0300

    Nova section PROJETOS

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     entry.target.classList.add("aparecendo");
-                    observer.unobserve(entry.target); 
+                    observer.unobserve(entry.target); // Remove do observer após a animação
                 }
             });
         },
@@ -14,65 +14,4 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     containers.forEach((container) => observer.observe(container));
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const sobreNos = document.querySelector("#sobre-nos");
-
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    sobreNos.classList.add("aparecer");
-                }
-            });
-        },
-        { threshold: 0.5 } 
-    );
-
-    observer.observe(sobreNos);
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-
-
-    let title = document.querySelector(".container-texto h2");
-    let text = title.innerText;
-    title.innerHTML = ""; 
-
-    let delay = 0; 
-    text.split("").forEach((letter) => {
-        let span = document.createElement("span");
-        span.innerText = letter;
-        
-        if (letter !== " ") {
-            span.style.animation = `fadeInLetter 0.1s ease-in forwards`;
-            span.style.animationDelay = `${delay}s`;
-            delay += 0.1; 
-        }
-
-        title.appendChild(span);
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const container = document.querySelector(".segundo-container");
-    const texto = document.querySelector(".containersegundo-texto");
-    const imagem = document.querySelector(".silas-samuel");
-
-    function checkScroll() {
-        const posicao = container.getBoundingClientRect().top;
-        const alturaTela = window.innerHeight;
-
-        if (posicao < alturaTela * 0.75) { // Quando 75% do container estiver visível
-            texto.classList.add("animar-esquerda");
-            imagem.classList.add("animar-direita");
-        }
-    }
-
-    window.addEventListener("scroll", checkScroll);
-    checkScroll(); // Para verificar se a seção já está visível ao carregar a página
 });
