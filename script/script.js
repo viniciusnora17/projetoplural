e764f4b5ae0fd645df1c77b28c2e57ef598b82d9
Vinicius Nora <vinicius.nora@sou.fae.br>
2025-04-11 23:27:26 -0300

    talvez um final

--- script/script.js
+++ script/script.js
@@ -1,21 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const containers = document.querySelectorAll(".container-escritorio");
-
-    const observer = new IntersectionObserver(
-        (entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("aparecendo");
-                    observer.unobserve(entry.target); 
-                }
-            });
-        },
-        { threshold: 0.2 }
-    );
-
-    containers.forEach((container) => observer.observe(container));
-});
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const sobreNos = document.querySelector("#sobre-nos");
